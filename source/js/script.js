fec330a9cbe1134c30f8bf04ba9edff5954c4b86
Alex Payareli <sashapayareli@gmail.com>
2019-03-12 16:58:56 +0200

    Finished mobile header

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+// Play btn js
 let playBtn = document.querySelector('.game__start-btn');
 let overlay = document.querySelector('.overlay');
 let registerModal = document.querySelector('.modal-register');
@@ -16,3 +18,14 @@ function toggleModal(evt) {
   overlay.classList.toggle('overlay--show');
   registerModal.classList.toggle('modal-register--show');
 }
+
+// Mobile nav js
+let navBtn = document.querySelector('.nav-btn');
+let mainNav = document.querySelector('.main-nav');
+
+navBtn.addEventListener('click', function(evt) {
+  evt.preventDefault;
+
+  navBtn.classList.toggle('nav-btn--opened');
+  mainNav.classList.toggle('main-nav--show');
+});
