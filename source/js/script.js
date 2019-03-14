d61106d37c9dc64e9851eb38471954b77b3d99b7
Alex Payareli <sashapayareli@gmail.com>
2019-03-14 17:05:52 +0200

    game-field in process

--- source/js/script.js
+++ source/js/script.js
@@ -2,22 +2,22 @@
 
 
 // Play btn js
-let playBtn = document.querySelector('.game__start-btn');
-let overlay = document.querySelector('.overlay');
-let registerModal = document.querySelector('.modal-register');
-let closeModal = document.querySelector('.modal-register__close-btn');
+// let playBtn = document.querySelector('.game__start-btn');
+// let overlay = document.querySelector('.overlay');
+// let registerModal = document.querySelector('.modal-register');
+// let closeModal = document.querySelector('.modal-register__close-btn');
 
 
-playBtn.addEventListener('click', toggleModal);
-overlay.addEventListener('click', toggleModal);
-closeModal.addEventListener('click', toggleModal);
+// playBtn.addEventListener('click', toggleModal);
+// overlay.addEventListener('click', toggleModal);
+// closeModal.addEventListener('click', toggleModal);
 
-function toggleModal(evt) {
-  evt.preventdefault;
+// function toggleModal(evt) {
+//   evt.preventdefault;
 
-  overlay.classList.toggle('overlay--show');
-  registerModal.classList.toggle('modal-register--show');
-}
+//   overlay.classList.toggle('overlay--show');
+//   registerModal.classList.toggle('modal-register--show');
+// }
 
 // Mobile nav js
 let navBtn = document.querySelector('.page-header__nav-btn');
