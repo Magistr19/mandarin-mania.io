0324d96aecc38c2f8e64c56c6ca93f890bd04a28
Alex Payareli <sashapayareli@gmail.com>
2019-03-20 17:44:45 +0200

    Js game-field

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,5 @@
 'use strict';
 
-// Play btn js
-// let playBtn = document.querySelector('.game__start-btn');
-// let overlay = document.querySelector('.overlay');
-// let registerModal = document.querySelector('.modal-register');
-// let closeModal = document.querySelector('.modal-register__close-btn');
-
-
-// playBtn.addEventListener('click', toggleModal);
-// overlay.addEventListener('click', toggleModal);
-// closeModal.addEventListener('click', toggleModal);
-
-// function toggleModal(evt) {
-//   evt.preventdefault;
-
-//   overlay.classList.toggle('overlay--show');
-//   registerModal.classList.toggle('modal-register--show');
-// }
-
 // Mobile nav js
 let navBtn = document.querySelector('.page-header__nav-btn');
 let mainNav = document.querySelector('.main-nav');
@@ -28,4 +10,42 @@ mainNav.addEventListener('click', toggleNav);
 function toggleNav(evt) {
     navBtn.classList.toggle('page-header__nav-btn--opened');
     mainNav.classList.toggle('main-nav--show');
+}
+
+// Game field on start js
+let playBtn = document.querySelector('.game__start-btn');
+let overlay = document.querySelector('.overlay');
+let registerModal = document.querySelector('.modal-register');
+let closeModal = document.querySelector('.modal-register__close-btn');
+
+let registerForm = document.querySelector('.modal-register__form');
+let registerSendBtn = document.querySelector('.modal-register__send-btn');
+
+let gameField = document.querySelector('.game__field');
+let gameStrtBtn = document.querySelector('.game__start-btn');
+let gameOptions = document.querySelector('.game__options');
+
+if (localStorage.getItem('nickName') && localStorage.getItem('userId')) { //Local storage check
+  gameField.classList.add('game__field--gaming');
+  gameStrtBtn.classList.add('custom-btn--hide');
+  gameOptions.classList.add('game-options--show');
+} else {
+  playBtn.addEventListener('click', toggleModal);
+  overlay.addEventListener('click', toggleModal);
+  closeModal.addEventListener('click', toggleModal);
+
+  function toggleModal(evt) {
+    evt.preventDefault();
+
+    overlay.classList.toggle('overlay--show');
+    registerModal.classList.toggle('modal-register--show');
+  }
+
+  registerForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+  });
+
+  registerSendBtn.addEventListener('submit', evt => {
+    evt.preventDefault();
+  });
 }
